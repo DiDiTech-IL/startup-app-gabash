491082ebcdf732d68fae2780e66b8815cbe7bed9
Yedidya Newlander <dev@didi-tech.com>
2026-02-24 18:40:47 +0200

    another try

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { z } = require("zod");
 const { prisma } = require("../lib/prisma");
 const { JWT_SECRET } = require("../middleware/auth");
